cff92b64dd22eeb3360b68455870d0470949c99b
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-08 11:54:18 +0000

    ActiveStrage導入

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,18 @@
 class Public::UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
