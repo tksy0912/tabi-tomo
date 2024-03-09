9b48482abf60c6a0db3e7c50582f90c6337d98f2
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-09 10:04:57 +0000

    新規登録ページ作成

--- app/controllers/public/schedules_controller.rb
+++ app/controllers/public/schedules_controller.rb
@@ -1,2 +1,18 @@
 class Public::SchedulesController < ApplicationController
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    @schedule.save
+    redirect_to public_trip_path(@schedule.trip_id)
+  end
+
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:date, :destination, :transportation, :is_accommodation, :accommodation_name, :trip_id)
+  end
 end
--- app/controllers/public/trips_controller.rb
+++ app/controllers/public/trips_controller.rb
@@ -1,2 +1,24 @@
 class Public::TripsController < ApplicationController
+  def new
+    @trip = Trip.new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user_id = current_user.id
+    @trip.save
+    redirect_to public_trip_path(@trip)
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+    @schedule = @trip.schedules
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title, :departure_date, :return_date, :user_id, :schedule_id)
+  end
 end
