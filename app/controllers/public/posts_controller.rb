dfa6934433dd0814488eebf11703b1ffa9523fae
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-11 17:28:58 +0000

    投稿機能大枠作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,19 +1,29 @@
 class Public::PostsController < ApplicationController
   def new
+    @trip = Trip.find(params[:trip_id])
     @post = Post.new
+    @post.trip_id = @trip.id
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to public_posts_path
+    redirect_to public_post_path(@post.id)
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :caption, :user_id, :trip_id)
+    params.require(:post).permit(:title, :caption, :user_id, :trip_id, :post_image)
   end
 end
