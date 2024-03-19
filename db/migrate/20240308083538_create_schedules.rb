bd9e6a025179d9a875326a96844613209ed76a79
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-19 11:05:55 +0000

    [Fix] migration schedules date

--- db/migrate/20240308083538_create_schedules.rb
+++ db/migrate/20240308083538_create_schedules.rb
@@ -2,7 +2,7 @@ class CreateSchedules < ActiveRecord::Migration[6.1]
   def change
     create_table :schedules do |t|
       t.integer :trip_id
-      t.date :date, null: false, default: ""
+      t.date :date, null: false
       t.text :destination, null: false, default: ""
       t.string :transportation, null: false, default: ""
       t.boolean :is_accommodation, null: false, default: true
