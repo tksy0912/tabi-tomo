9952688d2a2f4b061dbbf221fd5fcce11c4f525b
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-19 11:08:38 +0000

    [Fix] migration posts caption

--- db/migrate/20240308084357_create_posts.rb
+++ db/migrate/20240308084357_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.integer :user_id
       t.integer :trip_id
-      t.text :caption, null: false, default: ""
+      t.text :caption, null: false
 
       t.timestamps
     end
