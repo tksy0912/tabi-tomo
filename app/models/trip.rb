d8bcafd43807d6e4f3ea9e07a45c690fe4fb0032
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-20 04:41:58 +0000

    [Fix] tripとpostのアソシエーション

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ApplicationRecord
   has_many :schedules, dependent: :destroy
+  has_many :posts, dependent: :destroy
   accepts_nested_attributes_for :schedules, allow_destroy: true
   belongs_to :user
 end
