88eabc871445ebb0020e80831c31fdd92c11160e
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-20 05:58:53 +0000

    [Update] ログイン後遷移先変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    users_my_page_path(resource)
+  end
+
   def after_sign_out_path_for(resource)
     root_path
   end
