53a563728b2810acded3e0d5630c7f4690f02c61
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-25 10:00:46 +0000

    [Add] ページネーション追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -18,7 +18,7 @@ class Public::PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.page(params[:page]).per(10)
     @tag_list = PostTag.all
   end
 
