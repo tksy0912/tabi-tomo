934ad6df120c5cf342dc0d8ca4d73dd7851eeb37
EC2 Default User <takizawasayuki0912@gmail.com>
2024-03-28 04:50:12 +0000

    [Add] エラーメッセージ

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :comment, presence: true
+  validates :comment, length: { maximum: 50 }, presence: true
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,7 +4,7 @@ class Trip < ApplicationRecord
   accepts_nested_attributes_for :schedules, allow_destroy: true
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, length: { maximum: 20 }, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
 end
