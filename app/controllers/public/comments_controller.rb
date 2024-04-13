f0271135044dd142c8faf917edcf5052af408027
EC2 Default User <takizawasayuki0912@gmail.com>
2024-04-13 06:00:05 +0000

    [Add] コメント非同期機能追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,14 +1,16 @@
 class Public::CommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = post.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.new(comment_params)
     comment.user = current_user
     comment.save
 #    redirect_to request.referer
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
+    comment = Comment.find(params[:id])
+    @post = comment.post
+    comment.destroy
 #    redirect_to request.referer
   end
 
